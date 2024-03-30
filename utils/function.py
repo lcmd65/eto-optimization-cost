035b6800090e6d87b48192a9a1d0f5fb8171f3ed
Dat <dat.lemindast@gmail.com>
2024-03-30 13:06:05 +0700

    change

--- utils/function.py
+++ utils/function.py
@@ -2,11 +2,26 @@ import pandas as pd
 import numpy as np
 
 
-
+class ModelETO():
+    def __init__(self, cost_matrix, score_item_matrix):
+        self.cost = cost_matrix
+        self.score_item = score_item_matrix
+        
+    def create_matrix_costcase(self):
+        pass
+    
+    
+    def optimize(self):
+        pass
+    
+    def display(self):
+        pass
+        
 
 
 def create_model_optimize():
     """
     
     """
+    pass
     
